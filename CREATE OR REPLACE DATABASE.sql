3dbce2fb09cdc3ac0f523ae260cdb3c3d247024e
rsvmukhesh <91404171+rsvmukhesh@users.noreply.github.com>
2025-01-12 13:08:22 -0500

    Update CREATE OR REPLACE DATABASE.sql

--- CREATE OR REPLACE DATABASE.sql
+++ CREATE OR REPLACE DATABASE.sql
@@ -4,8 +4,8 @@ CREATE OR REPLACE STORAGE INTEGRATION spotify_s3_int
     TYPE = EXTERNAL_STAGE
     STORAGE_PROVIDER = S3
     ENABLED = TRUE
-    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::947924525390:role/s3-snowflake'
-    STORAGE_ALLOWED_LOCATIONS = ('s3://spotify-etl-project-rsvm')
-     COMMENT = 'creating connection to s3'
+    STORAGE_AWS_ROLE_ARN = '*******'
+    STORAGE_ALLOWED_LOCATIONS = ('*****')
+     
 
 DESC INTEGRATION spotify_s3_int;
